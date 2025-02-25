cff9635d15e9a914456f10a3f1b5f07af53055ee
Horus Orion Pacheco Marcano <horuspacheco@gmail.com>
2025-02-25 10:21:58 -0400

    Arreglando codigos de paises

--- src/app/services/country.codes.js
+++ src/app/services/country.codes.js
@@ -1,29 +1,7 @@
 export const getCountryCodes = () => {
     return [
-      { code: "1", name: "USA", flag: "🇺🇸" },
       { code: "58", name: "Venezuela", flag: "🇻🇪" },
-      { code: "34", name: "España", flag: "🇪🇸" },
-      { code: "52", name: "México", flag: "🇲🇽" },
-      { code: "54", name: "Argentina", flag: "🇦🇷" },
-      { code: "55", name: "Brasil", flag: "🇧🇷" },
-      { code: "44", name: "Reino Unido", flag: "🇬🇧" },
-      { code: "49", name: "Alemania", flag: "🇩🇪" },
-      { code: "33", name: "Francia", flag: "🇫🇷" },
-      { code: "39", name: "Italia", flag: "🇮🇹" },
-      { code: "81", name: "Japón", flag: "🇯🇵" },
-      { code: "86", name: "China", flag: "🇨🇳" },
-      { code: "7", name: "Rusia", flag: "🇷🇺" },
-      { code: "91", name: "India", flag: "🇮🇳" },
-      { code: "82", name: "Corea del Sur", flag: "🇰🇷" },
-      { code: "61", name: "Australia", flag: "🇦🇺" },
-      { code: "27", name: "Sudáfrica", flag: "🇿🇦" },
-      { code: "31", name: "Países Bajos", flag: "🇳🇱" },
-      { code: "47", name: "Noruega", flag: "🇳🇴" },
-      { code: "46", name: "Suecia", flag: "🇸🇪" },
-      { code: "41", name: "Suiza", flag: "🇨🇭" },
-      { code: "90", name: "Turquía", flag: "🇹🇷" },
-      { code: "62", name: "Indonesia", flag: "🇮🇩" },
-      { code: "63", name: "Filipinas", flag: "🇵🇭" }
+      {code : '598', name : "Uruguay", flag : 'UY'}
     ];
   };
   
