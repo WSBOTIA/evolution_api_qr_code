4b8633ff9f069d969e43a3e7744a06d5ab8d8b3b
Horus Orion Pacheco Marcano <horuspacheco@gmail.com>
2025-02-28 10:32:20 -0400

    Mejora visual

--- src/app/page.js
+++ src/app/page.js
@@ -2,9 +2,10 @@
 
 import { useEffect, useState } from "react";
 import React from 'react';
-import { 
-  CToast, CToastBody, CToastClose, CToaster, CContainer, CRow, CCol, 
-  CForm, CFormLabel, CFormSelect, CFormInput, CButton, CImage 
+import {
+  CToast, CToastBody, CToastClose, CToaster, CContainer, CRow, CCol,
+  CForm, CFormLabel, CFormSelect, CFormInput, CButton, CImage,
+  CCard
 } from '@coreui/react';
 import { connectionState, fetchInstances, getQr } from "./services/evolution.api.service";
 import { getCountryCodes } from "./services/country.codes";
@@ -17,6 +18,7 @@ export default function Home() {
   const [qr, setQR] = useState('/qr_code.jpg');
   const [selectedInstance, setSelectedInstance] = useState(null);
   const [instances, setInstances] = useState([]);
+  const [countdown, setCountdown] = useState(45);
 
   useEffect(() => {
     const loadInstances = async () => {
@@ -37,18 +39,28 @@ export default function Home() {
   }, []);
 
   useEffect(() => {
-    let interval;
     if (selectedInstance && (selectedInstance.state === 'close' || selectedInstance.state === 'connecting')) {
-      interval = setInterval(async () => {
+      const interval = setInterval(async () => {
         const { success, response } = await getQr(selectedInstance.name);
         if (success) {
           setQR(response.data.base64);
+          setCountdown(45);
         } else {
           showToast("Error al actualizar el código QR", 'danger');
         }
       }, 45000);
+
+      return () => clearInterval(interval);
     }
-    return () => clearInterval(interval);
+  }, [selectedInstance]);
+
+  useEffect(() => {
+    if (selectedInstance && (selectedInstance.state === 'close' || selectedInstance.state === 'connecting')) {
+      const countdownInterval = setInterval(() => {
+        setCountdown(prev => (prev > 0 ? prev - 1 : 0));
+      }, 1000);
+      return () => clearInterval(countdownInterval);
+    }
   }, [selectedInstance]);
 
   const showToast = (message, color) => {
@@ -71,11 +83,12 @@ export default function Home() {
         if (success) {
           const instanceState = response.data.instance.state;
           setSelectedInstance(prev => ({ ...prev, state: instanceState }));
-          
+
           if (instanceState === 'close' || instanceState === 'connecting') {
             const { success: qrSuccess, response: qrResponse } = await getQr(foundInstance.name);
             if (qrSuccess) {
               setQR(qrResponse.data.base64);
+              setCountdown(45);
             } else {
               showToast("Error al obtener el código QR", 'danger');
             }
@@ -95,44 +108,51 @@ export default function Home() {
   return (
     <>
       <CContainer className="vh-100 d-flex align-items-center justify-content-center">
-        <CRow className="w-100">
-          <CCol md={4} className="p-4 border-end">
-            <h5>Ingrese su número</h5>
-            <CForm onSubmit={handleSubmit}>
-              <CFormLabel htmlFor="countrySelect">Código del País</CFormLabel>
-              <CFormSelect id="countrySelect" value={countryCode} onChange={(e) => setCountryCode(e.target.value)}>
-                {countryList.map(({ code, flag, name }) => (
-                  <option key={code} value={code}>{flag} {name} (+{code})</option>
-                ))}
-              </CFormSelect>
+        <CRow className="w-100 d-flex align-items-center justify-content-center">
+          <CCol md={5} className="p-4 border-end ">
 
-              <CFormLabel htmlFor="phoneInput" className="mt-3">Número de Teléfono</CFormLabel>
-              <CFormInput
-                id="phoneInput"
-                type="tel"
-                placeholder="Ej: 4148315972"
-                value={phoneNumber}
-                onChange={(e) => setPhoneNumber(e.target.value.replace(/\D/, ''))}
-                maxLength={10}
-              />
 
-              <CButton type="submit" color="primary" className="mt-3 w-100">
-                Enviar
-              </CButton>
-            </CForm>
+            <CCard id="form-container" style={{ width: '70%' }}>
+              <h4  class="text-center">INGRESE SU NUMERO</h4>
+              <CForm onSubmit={handleSubmit}>
+                <CFormLabel htmlFor="countrySelect"></CFormLabel>
+                <CFormSelect id="countrySelect" value={countryCode} onChange={(e) => setCountryCode(e.target.value)}>
+                  {countryList.map(({ code, flag, name }) => (
+                    <option key={code} value={code}>{name} (+{code})</option>
+                  ))}
+                </CFormSelect>
+
+                <CFormLabel htmlFor="phoneInput" className="mt-3"></CFormLabel>
+                <CFormInput
+                  id="phoneInput"
+                  type="tel"
+                  placeholder="Ej: 4148315972"
+                  value={phoneNumber}
+                  onChange={(e) => setPhoneNumber(e.target.value.replace(/\D/, ''))}
+                  maxLength={10}
+                />
+
+                <CButton type="submit" className="w-100 button-submit">
+                  OBTENER CODIGO QR
+                </CButton>
+              </CForm>
+            </CCard>
           </CCol>
 
-          <CCol md={8} className="p-4 d-flex flex-column align-items-center justify-content-center">
+          <CCol id="col-qr" md={7} className="p-4 d-flex flex-column align-items-center justify-content-center">
             {selectedInstance ? (
               <>
-                <h2>Bienvenido {selectedInstance.name}</h2>
-                {selectedInstance.state === 'close' || selectedInstance.state === 'connecting' ? (
+                <h2 id="title-qr">Bienvenido {selectedInstance.name}</h2>
+                {(selectedInstance.state === 'close' || selectedInstance.state === 'connecting') ? (
                   <>
-                    <p>Escanea el código QR para conectar con WhatsApp</p>
-                    <CImage rounded src={qr} width={300} height={300} />
+                    <p class="text-qr">Escanea el código QR en {countdown} segundos</p>
+                    <br/>
+                    <CImage id="qr" rounded src={qr} width={300} height={300} />
+                    <br/><br/>
+                    <p class="text-qr">Al culminarse el tiempo se generará un nuevo codigo QR</p>
                   </>
                 ) : (
-                  <p>Ya estás conectado</p>
+                  <p class="text-qr">Ya estás conectado</p>
                 )}
               </>
             ) : (
